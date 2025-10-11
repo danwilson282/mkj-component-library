3814cdd365406c3e55372c8d60bbe0fc210c8253
Dan Wilson <danwilson282@yahoo.co.uk>
2025-10-11 11:07:26 +0100

    added jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'jsdom', // needed for React components
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest', // use ts-jest for TS files
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // optional setup file
+};
