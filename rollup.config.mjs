0f0dfcbaaa24506ebdc95659038ec55bb3ea3944
Dan Wilson <danwilson282@yahoo.co.uk>
2025-10-11 12:40:00 +0100

    added config to be able to publish locally

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
 import typescript from "rollup-plugin-typescript2";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import pkg from "./package.json";
+import pkg from "./package.json" with { type: 'json' };;
 
 export default {
   input: "src/index.ts",
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ export interface ButtonProps {
   onClick?: () => void;
 }
 
- const Button: React.FC<ButtonProps> = ({ label, onClick }) => (
+export const Button: React.FC<ButtonProps> = ({ label, onClick }) => (
   <button
     className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
     onClick={onClick}
@@ -14,4 +14,4 @@ export interface ButtonProps {
   </button>
 );
 
-export default Button;
+// export default Button;
