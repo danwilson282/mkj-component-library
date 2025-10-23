292c579330fd540d72f8424b2cf1b837de432f04
Dan Wilson <danwilson282@yahoo.co.uk>
2025-10-23 20:51:30 +0100

    version

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,39 +1,26 @@
 import typescript from "rollup-plugin-typescript2";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import pkg from "./package.json" with { type: 'json' };;
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import fs from "fs";
-const pkg = JSON.parse(fs.readFileSync(new URL("./package.json", import.meta.url)));
 
 export default {
   input: "src/index.ts",
   output: [
-    {
-      file: pkg.main,        // CommonJS
-      format: "cjs",
-      sourcemap: true,
-      exports: "named",      // ensures named exports work
-    },
-    {
-      file: pkg.module,      // ES Module
-      format: "esm",
-      sourcemap: true,
-    },
+    { file: pkg.main, format: "cjs", sourcemap: true },
+    { file: pkg.module, format: "esm", sourcemap: true },
   ],
-  external: ["react", "react-dom"],
+  external: ['react', 'react-dom'],
   plugins: [
     peerDepsExternal(),
     resolve(),
-    commonjs({
-      include: /node_modules/,
-      requireReturnsDefault: "auto", // helps with CJS interop
-    }),
-    typescript({
-      tsconfig: "./tsconfig.json",
-      useTsconfigDeclarationDir: true,
+    commonjs(),
+    typescript({ 
+      tsconfig: './tsconfig.json',
+      useTsconfigDeclarationDir: true ,
       tsconfigOverride: {
         exclude: ["**/*.stories.tsx"],
       },
-    }),
+    })
   ],
 };
