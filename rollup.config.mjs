de9af17bda4add295bdbffd04e585f0924c2bc0e
Dan Wilson <danwilson282@yahoo.co.uk>
2025-10-23 20:37:12 +0100

    commit

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,8 @@ import typescript from "rollup-plugin-typescript2";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import pkg from "./package.json" assert { type: "json" };
+import fs from "fs";
+const pkg = JSON.parse(fs.readFileSync(new URL("./package.json", import.meta.url)));
 
 export default {
   input: "src/index.ts",
